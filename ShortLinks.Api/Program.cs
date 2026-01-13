49acbd00bafd4c39a9104342a55e131af2151e77
Mohamed Erfan <mzkamel@2pservice.com>
2026-01-13 13:22:14 +0300

    add redis with version 1.0.0

--- ShortLinks.Api/Program.cs
+++ ShortLinks.Api/Program.cs
@@ -1,7 +1,5 @@
-﻿
-using Microsoft.EntityFrameworkCore;
-using ShortLinks.Core;
-using ShortLinks.Storage.EFCore;
+﻿using ShortLinks.Core;
+using ShortLinks.Storage.Redis;
 
 namespace ShortLinks.Api
 {
@@ -19,25 +17,39 @@ namespace ShortLinks.Api
             builder.Services.AddSwaggerGen();
 
             // ✅ 1) Register ShortLinks Core services (service + code generator + options)
+            //builder.Services.AddShortLinksCore(opt =>
+            //{
+            //    opt.BaseUrl = "https://localhost:7111/s"; // what will be returned in Create result
+            //    opt.DefaultCodeLength = 8;
+            //    opt.MaxCreateAttempts = 15;
+            //    opt.TrackHits = true;
+            //});
+
+            //// ✅ 2) Register EF Core SQL Server store
+            //builder.Services.AddShortLinksEfCore(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ShortLinksDb")));
+
             builder.Services.AddShortLinksCore(opt =>
             {
-                opt.BaseUrl = "https://localhost:7111/s"; // what will be returned in Create result
-                opt.DefaultCodeLength = 8;
-                opt.MaxCreateAttempts = 15;
+                opt.BaseUrl = "https://localhost:7111/s";
                 opt.TrackHits = true;
             });
 
-            // ✅ 2) Register EF Core SQL Server store
-            builder.Services.AddShortLinksEfCore(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ShortLinksDb")));
+            builder.Services.AddShortLinksRedis(
+            builder.Configuration["Redis:ConnectionString"]!,
+            opt =>
+            {
+                opt.KeyPrefix = "shortlinks:";
+                opt.UseKeyTtlForExpiration = true;
+            });
 
             var app = builder.Build();
 
-            // ✅ 3) Apply migrations automatically (recommended)
-            using (var scope = app.Services.CreateScope())
-            {
-                var db = scope.ServiceProvider.GetRequiredService<ShortLinksDbContext>();
-                db.Database.Migrate();
-            }
+            //// ✅ 3) Apply migrations automatically (recommended)
+            //using (var scope = app.Services.CreateScope())
+            //{
+            //    var db = scope.ServiceProvider.GetRequiredService<ShortLinksDbContext>();
+            //    db.Database.Migrate();
+            //}
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
